ea25b6b2469edde836a63de1b76b26f7a09c2be1
YuMentsel <101459634+YuMentsel@users.noreply.github.com>
2022-11-07 23:58:33 +0300

    fix: adjust script for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fsPr = require('fs/promises');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 let dirPath = path.join(__dirname, 'files');
@@ -8,24 +8,21 @@ let newDirPath = path.join(__dirname, 'files-copy');
 // Async/await (это не SYNC!) /////////////////////////
 
 async function copyDir() {
-  await fsPr.rm(newDirPath, { force: true, recursive: true }, err => { if(err) throw err });
-  await fsPr.mkdir(newDirPath, err => { if(err) throw err });
-  fs.readdir(dirPath, { withFileTypes: true }, (err, dir) => {
-    if (err) throw err;
-    dir.forEach( file => {
-      if (file.isFile()) {
-        fs.copyFile(path.join(dirPath, file.name), path.join(newDirPath, file.name), err => { if (err) throw err });
-      }
-    });
+  await fsPromises.rm(newDirPath, { force: true, recursive: true });
+  await fsPromises.mkdir(newDirPath);
+  let dir = await fsPromises.readdir(dirPath, { withFileTypes: true });
+  dir.forEach( file => {
+    if (file.isFile()) {
+      fs.copyFile(path.join(dirPath, file.name), path.join(newDirPath, file.name), err => { if (err) throw err });
+    }
   });
-}
+};
 
 copyDir();
 
 // callback /////////////////////////////////////////////
 
 // const fs = require('fs');
-// const fsPr = require('fs/promises');
 // const path = require('path');
 
 // let dirPath = path.join(__dirname, 'files');
