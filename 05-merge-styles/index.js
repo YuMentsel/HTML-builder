73761f8974255e03988ddbc60a44a0191d563bf9
YuMentsel <101459634+YuMentsel@users.noreply.github.com>
2022-11-07 23:47:47 +0300

    fix: adjust script for 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const stylesPath =  path.join(path.join(__dirname, 'styles'));
 const bundlePath =  path.join(path.join(__dirname, 'project-dist', 'bundle.css'));
-const output = fs.createWriteStream(bundlePath, 'utf-8');
+const output = fs.createWriteStream(bundlePath);
 
-fs.promises.readdir(stylesPath, { withFileTypes: true })
-.then (files => files.forEach(file => {
-  if (file.isFile() && path.extname(path.join(stylesPath, file.name)) === '.css') {
-    const input = fs.createReadStream(path.join(stylesPath, file.name), 'utf8');
-    input.pipe(output);
-  }
-}));
+async function createBundle() {
+  const files = await fsPromises.readdir(stylesPath, { withFileTypes: true });
+  files.forEach(file => {
+    if (file.isFile() && path.extname(path.join(stylesPath, file.name)) === '.css') {
+      const input = fs.createReadStream(path.join(stylesPath, file.name), 'utf8');
+      input.pipe(output);
+    }
+  })
+}
+
+createBundle();
